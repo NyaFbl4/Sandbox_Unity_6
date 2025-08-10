d242cf749d50309a5e4bcb5ac469ebc9f815e31a
IzmaylovNikita <nikitaizmaylov1402@gmail.com>
2025-08-10 16:59:25 +0300

    Начал реализовывать данные героя

---
+++ Assets/Scripts/Data/HeroController.cs
@@ -0,0 +1,10 @@
+﻿namespace Data
+{
+    public class HeroController
+    {
+        private HeroData _heroData;
+
+        private int _increaseHealth;
+        private int _increaseAttack;
+    }
+}
---
+++ Assets/Scripts/Data/HeroData.cs
@@ -0,0 +1,9 @@
+﻿namespace Data
+{
+    public class HeroData
+    {
+        public int Lvl { get; set; }
+        public int Health { get; set; }
+        public int Attack { get; set; }
+    }
+}
